6f88de9abd080a1179759d17f275765d381b2889
peterdeme <demepeter93@gmail.com>
2026-02-22 13:53:39 +0100

    fix: add gosec directive to JWT field comment in user struct

--- client/session/user.go
+++ client/session/user.go
@@ -1,6 +1,6 @@
 package session
 
 type user struct {
-	JWT        string `graphql:"jwt"`
+	JWT        string `graphql:"jwt"` //nolint:gosec // this is not a stored secret
 	ValidUntil int64  `graphql:"validUntil"`
 }
